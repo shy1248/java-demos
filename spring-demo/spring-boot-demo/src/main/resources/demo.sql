bfa2e85d5d254e3f28c46912f387ff3938ccaa18
yushuibo <hengchen2005@gmail.com>
2021-03-30 23:03:12 +0800

    remove ignored files

--- spring-demo/spring-boot-demo/src/main/resources/demo.sql
+++
@@ -1,7 +0,0 @@
--- sqlite3 demos.db
-
-create table girl (
-	id int primary key not null,
-	cupsize varchar(1),
-	age int default 18
-);
--- spring-demo/spring-framework-demo/src/main/resources/demo.sql
+++
@@ -1,7 +0,0 @@
--- sqlite3 demos.db
-
-create table user (
-	id int primary key not null,
-	names varchar(20),
-	age int default 18
-);
